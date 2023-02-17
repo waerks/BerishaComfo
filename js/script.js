553f6b867cb5e118aeef7bf76aae6425e191b892
Anaïs <a.waerks@gmail.com>
2023-02-17 11:41:15 +0100

    Push

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    // AJAX
+    $.ajax({
+        url: '',
+
+        type: 'GET',
+
+        data: {},
+
+        dataType: 'json',
+
+        success: function(data){
+            
+        }
+    })
+
+})
